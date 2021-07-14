0e681f3259304ac57c206fb0b4a1859932468884
Andre Moraes <andre-altm@hotmail.com>
2021-07-14 14:15:02 -0300

    estrutura básica

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import * as S from './styled';
 
 function App() {
   return (
-    <h1>Garagem 1973</h1>
+    <S.content>
+      <S.landingSection>
+        <h1 style={{color: '#fff'}}>landingSection</h1>
+      </S.landingSection>
+      <S.productsSection>
+        <h1>productsSection</h1>
+      </S.productsSection>
+      <S.footerSection>
+        <h1 style={{color: '#fff'}}>footerSection</h1>
+      </S.footerSection>
+      <S.creditSection>
+        <span>Desenvolvido por <S.link href='https://github.com/andreLTMoraes/' target='_blank' rel='noreferrer'>André Moraes</S.link></span>
+      </S.creditSection>
+    </S.content>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+export const content = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100vw;
+`;
+
+export const landingSection = styled.section`
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+`;
+
+export const productsSection = styled.section`
+    width: 100vw;
+`;
+
+export const footerSection = styled.section`
+    width: 100vw;
+    background-color: #000;
+`;
+
+export const creditSection = styled.section`
+    width: 100vw;
+    display: flex;
+    padding: .5rem 0;
+    justify-content: center;
+    background-color: #333;
+    color: #fff;
+`;
+
+export const link = styled.a`
+    color: #fff;
+    text-decoration: none;
+    
+`;
